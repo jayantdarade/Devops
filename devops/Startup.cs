e32260e7e8dae58fcfc2b1cddca499972d9633b0
jntdarade <jntdarade@gmail.com>
2020-05-29 16:12:32 +0530

    devopsmvcproject

---
+++ devops/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(devops.Startup))]
+namespace devops
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
